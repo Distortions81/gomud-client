c000baa9052737bb35f0c7b6bd6e8aa6054627fd
Distortions81 <carlotto81@gmail.com>
2021-07-06 15:42:45 -0600

    prerender start

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"image/color"
 	"log"
+	"math"
 
 	_ "github.com/flopp/go-findfont"
 	"github.com/golang/freetype/truetype"
@@ -29,11 +30,15 @@ var greeting []byte
 //Constants
 const MAX_INPUT_LENGTH = 1024 * 100
 const MAX_LINE_LENGTH = 1024
+
+const MAX_SCROLL_LINES = 10000
 const MAX_VIEW_LINES = 512
 
 const defaultWindowTitle = "GoMud-Client"
 const defaultServer = "127.0.0.1:7778"
 const VersionString = "Pre-Alpha build, v0.0.02 07052021937p"
+
+const defaultHorizontalSpaceRatio = 1.5
 
 const defaultWindowWidth = 960
 const defaultWindowHeight = 540
@@ -41,8 +46,6 @@ const defaultUserScale = 1.0
 
 const defaultRepeatInterval = 3
 const defaultRepeatDelay = 30
-
-const MAX_SCROLL_LINES = 10000
 
 type Window struct {
 	serverAddr  string
@@ -61,31 +64,27 @@ type Window struct {
 	repeatDelay    int
 	repeatInterval int
 
-	scrollBack ScrollBack
-	viewport   ViewPort
-
-	viewPortStrings [MAX_VIEW_LINES]string
-	viewPortPixels  [MAX_VIEW_LINES]string
+	lines TextHistory
 
 	dirty bool
 }
 
-type ScrollBack struct {
-	lines [MAX_SCROLL_LINES]string
-	pos   int
-	head  int
-	tail  int
-}
-
-type ViewPort struct {
+type TextHistory struct {
+	lines    [MAX_SCROLL_LINES]string
+	pixLines [MAX_SCROLL_LINES]*ebiten.Image
+	rendered [MAX_SCROLL_LINES]bool
+	pos      int
+	head     int
+	tail     int
 }
 
 type FontData struct {
-	vertSpace int
-	charWidth int
-	size      float64
-	data      []byte
-	face      font.Face
+	vertSpace  int
+	charWidth  int
+	chatHeight int
+	size       int
+	data       []byte
+	face       font.Face
 
 	dirty bool
 }
@@ -112,9 +111,31 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return outsideWidth, outsideHeight
 }
 
-func (g *Game) Update() error {
+func renderText() {
+	mainWin.lines.pixLines[0] = renderLine(0)
+}
 
-	RenderOffscreen()
+func renderLine(pos int) *ebiten.Image {
+	if mainWin.realWidth > 0 && mainWin.font.size > 0 {
+
+		tempImg := ebiten.NewImage(mainWin.realWidth, int(mainWin.font.size)+mainWin.font.vertSpace)
+		text.Draw(tempImg, mainWin.lines.lines[pos],
+			mainWin.font.face,
+			0,
+			0,
+			color.RGBA{0xFF, 0x00, 0x00, 0xFF})
+
+		mainWin.dirty = true
+		mainWin.lines.rendered[pos] = true
+		return tempImg
+	}
+	mainWin.lines.rendered[pos] = false
+	return nil
+}
+
+func (g *Game) Update() error {
+	renderText()
+	renderOffscreen()
 	return nil
 }
 
@@ -149,45 +170,41 @@ func init() {
 	//Init font
 	mainWin.font.size = defaultFontSize
 	mainWin.font.face = truetype.NewFace(tt, &truetype.Options{
-		Size:              mainWin.font.size,
+		Size:              float64(mainWin.font.size),
 		Hinting:           font.HintingFull,
 		GlyphCacheEntries: glyphCacheSize,
 	})
 
-	mainWin.scrollBack.lines[0] = ""
-	mainWin.scrollBack.pos = 0
-	mainWin.scrollBack.head = 0
-	mainWin.scrollBack.tail = 0
+	//Calculate character pixel width
+	mainWin.font.charWidth = int(math.Round(float64(mainWin.font.size) / float64(defaultHorizontalSpaceRatio)))
+
+	mainWin.lines.lines[0] = "This"
+	mainWin.lines.lines[1] = "is"
+	mainWin.lines.lines[2] = "a"
+	mainWin.lines.lines[3] = "test"
+	mainWin.lines.pos = 0
+	mainWin.lines.head = 0
+	mainWin.lines.tail = 3
 
 	mainWin.offScreen = ebiten.NewImage(mainWin.width, mainWin.height)
-	mainWin.dirty = true
+	mainWin.dirty = false
 
 	ebiten.SetWindowTitle(mainWin.title)
 	ebiten.SetWindowSize(mainWin.width, mainWin.height)
 	ebiten.SetMaxTPS(60)
 }
 
-func ScrollbackToViewport() {
-
-}
-
-func PreRenderLines() {
-
-}
-
-func RenderOffscreen() {
+func renderOffscreen() {
 	if mainWin.dirty {
 		mainWin.dirty = false
 
 		mainWin.offScreen.Clear()
 		mainWin.offScreen.Fill(color.RGBA{0x30, 0x00, 0x00, 0xFF})
 
-		charColor := color.RGBA{0xFF, 0x00, 0x00, 0xFF}
-		text.Draw(mainWin.offScreen, "testing",
-			mainWin.font.face,
-			50,
-			50,
-			charColor)
+		//Render our images out here
+		if mainWin.lines.rendered[0] == true {
+			mainWin.offScreen.DrawImage(mainWin.lines.pixLines[0], nil)
+		}
 	}
 }
 
@@ -201,7 +218,7 @@ func (g *Game) Draw(screen *ebiten.Image) {
 
 		//Re-render
 		mainWin.dirty = true
-		RenderOffscreen()
+		renderOffscreen()
 		fmt.Println("Buffer resized.")
 	}
 
