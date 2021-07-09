2c440ace9ee95cff4a947626c7daa5efca3aaf2c
Distortions81 <carlotto81@gmail.com>
2021-07-09 00:33:14 -0600

    newline

--- main.go
+++ main.go
@@ -290,7 +290,7 @@ func init() {
 
 	addLine(
 		"GOMud-Client " + VersionString + "\n" +
-			"COPYRIGHT 2020-2021 Carl Frank Otto III (carlotto81@gmail.com)" +
+			"COPYRIGHT 2020-2021 Carl Frank Otto III (carlotto81@gmail.com)\n" +
 			"License: Mozilla Public License 2.0\n" +
 			"Written in Go, using Ebiten library.\n" +
 			"This information must remain unmodified, fully intact and shown to end-users.\n" +
